bbd3c3ad5eb5526fd44908ef00221b17a07a57d2
Remi Delaporte-Mathurin <delaporte.mathurin@laposte.net>
2018-08-02 13:46:51 +0100

    Identified the error its the coupling

--- 1D_trapping.py
+++ 1D_trapping.py
@@ -36,8 +36,8 @@ print(num_steps)
 
 
 print('Defining mesh')
-nodes=5000
-size=1e-6#2.5e-7
+nodes=500
+size=3e-6
 #Dx=size/nodes
 mesh = IntervalMesh(nodes,0,size)
 
@@ -49,9 +49,10 @@ V0 = FunctionSpace(mesh, 'DG', 0) #FunctionSpace of the materials properties
 print('Defining initial values')
 
 ##Tritium concentration
-iniC = Expression('0.0',degree=1)
-c_sol_n = interpolate(iniC, V)
-c_trap_n = interpolate(Constant(0.0),V)
+iniC = Expression('0',degree=1)
+#c_sol_n = interpolate(iniC, V)
+c_sol_n = interpolate(Constant(0),V)
+c_trap_n = interpolate(Constant(0),V)
 c_trap2_n = interpolate(Constant(0.0),V)
 c_trap3_n = interpolate(Constant(0.0),V)
 n3_n=interpolate(Constant(0.0),V) #trap 3 density at t=0s
@@ -95,7 +96,7 @@ temp=Expression('t <= (implantation_time+resting_time) ? 300: 300+8*(t-(implanta
 print('Defining the materials properties')
 
 
-k_B=8.6173303e-5 #Boltzann constant eV/K
+k_B=8.6e-5 #Boltzann constant eV/K
 density_W=6e28 #density of tungsten in m^-3
 def calculate_D(T,subdomain_no):
   R=8.314 #Perfect gas constant
@@ -113,55 +114,77 @@ for cell in cells(mesh):
 
 
 alpha=110e-12 #lattice constant ()
-beta=6*density_W#number of solute sites per atom (6 for W)
+beta=6#*density_W#number of solute sites per atom (6 for W)
 v_0=1e13#frequency factor s-1
 E1=0.87 #in eV trap 1 activation energy
-n1=1e-3*density_W#trap 1 density
+n1=1e-3#*density_W#trap 1 density
+#n1=Expression('0+1*t',t=t,degree=1)#*density_W#trap 1 density
+
 E2=1.0 #in eV activation energy
-n2=4e-4*density_W #trap 2 density
+n2=4e-4#*density_W #trap 2 density
 E3=1.5 #in eV activation energy
 xp=1e-6
 teta=Expression('x[0]<=xp ? 1/xp :0',xp=xp,degree=2)
-n3amax=1e-1*density_W
+n3amax=1e-1#*density_W
 n3a=6e-4#*density_W
-n3bmax=1e-2*density_W
+n3bmax=1e-2#*density_W
 n3b=2e-4#*density_W
 ### Define variational problem
 print('Defining the variational problem')
-
-c_sol = TrialFunction(V)#c is the tritium concentration
-c_trap = TrialFunction(V)#c is the tritium concentration
-c_trap2 = TrialFunction(V)#c is the tritium concentration
-c_trap3 = TrialFunction(V)#c is the tritium concentration
-n3 = TrialFunction(V)#n3 is the density of trap 3
-vc = TestFunction(V)
-
 phi=Expression('t<=implantation_time ? flux: 0',implantation_time=implantation_time,flux=flux,t=0,degree=2)#this is the incident flux in D/m2/s      
 r=0.56
 fx = Expression('1/(width*pow(2*3.14,0.5))*exp(-0.5*(pow((x[0]-center)/width,2)))',center=4.5e-9,width=2.5e-9,degree=2)#  This is the tritium volumetric source term   -1/(1/3*e*pow(2*3.14,0.5))*exp(-0.5*(x[0]/pow(1/3*e,2)))   (x[0]<e/100 ? -2.5e19*(1-100*x[0]/e)
+vc = TestFunction(V)
+vc1 = TestFunction(V)
+vc2 = TestFunction(V)
 
-F1=((c_sol-c_sol_n)/dt)*vc*dx + D*dot(grad(c_sol), grad(vc))*dx -(1-r)*phi*fx*vc*dx +(c_trap-c_trap_n)/dt*vc*dx+(c_trap2-c_trap2_n)/dt*vc*dx+(c_trap3-c_trap3_n)/dt*vc*dx
-ac1,Lc1= lhs(F1),rhs(F1)
-
-F2=((c_trap-c_trap_n)/dt)*vc*dx - D/(alpha**2*beta)*c_sol_n*(n1-c_trap)*vc*dx + c_trap*v_0*exp(-E1/(k_B*temp))*vc*dx
-ac2,Lc2= lhs(F2),rhs(F2)
-
-F3=((c_trap2-c_trap2_n)/dt)*vc*dx - D/(alpha**2*beta)*c_sol_n*(n2-c_trap2)*vc*dx + c_trap2*v_0*exp(-E2/(k_B*temp))*vc*dx
-ac3,Lc3= lhs(F3),rhs(F3)
-
-F4=((c_trap3-c_trap3_n)/dt)*vc*dx - D/(alpha**2*beta)*c_sol_n*(n3_n-c_trap3)*vc*dx + c_trap3*v_0*exp(-E3/(k_B*temp))*vc*dx
-ac4,Lc4= lhs(F4),rhs(F4)
-
-Fn3=((n3-n3_n)/dt)*vc*dx-(1-r)*phi*((1-n3/n3amax)*n3a*fx+(1-n3/n3bmax)*n3b*teta)*vc*dx
-acn3,Lcn3=lhs(Fn3),rhs(Fn3)
-
-### Time-stepping
-
-c_sol=Function(V)
+c_sol = Function(V)
 c_trap=Function(V)
 c_trap2=Function(V)
 c_trap3=Function(V)
 n3=Function(V)
+A=Function(V)
+B=Function(V)
+
+
+
+
+c_sol = TrialFunction(V)#c is the tritium concentration
+F1=((c_sol-c_sol_n)/dt)*vc*dx  +4.1e-7*exp(-0.39/(k_B*temp))/(2**0.5)*dot(grad(c_sol), grad(vc))*dx -(1-r)*phi*fx/density_W*vc*dx+ (c_trap-c_trap_n)/dt*vc*dx +(c_trap2-c_trap2_n)/dt*vc*dx#+(c_trap2-c_trap2_n)/dt*vc*dx+(c_trap3-c_trap3_n)/dt*vc*dx         4.1e-7*exp(-0.39/(k_B*temp))/(2**0.5)*dot(grad(c_sol), grad(vc))*dx    
+ac1,Lc1= lhs(F1),rhs(F1)
+c_sol=Function(V)
+
+c_trap = TrialFunction(V)#c is the tritium concentration
+F2=((c_trap-c_trap_n)/dt)*vc*dx -4.1e-7*exp(-0.39/(k_B*temp))/(2**0.5)/((alpha**2)*beta)*n1*c_sol_n*(1-c_trap/n1)*vc*dx  + c_trap*v_0*exp(-E1/(k_B*temp))*vc*dx#-1*vc1*dx#    4.1e-7*exp(-0.39/(k_B*300))/(2**0.5)/((alpha**2)*beta)*n1*
+ac2,Lc2= lhs(F2),rhs(F2)
+c_trap=Function(V)
+
+print(4.1e-7*exp(-0.39/(k_B*300))/(2**0.5)/((alpha**2)*beta)*n1)
+
+
+c_trap2 = TrialFunction(V)#c is the tritium concentration
+F3=((c_trap2-c_trap2_n)/dt)*vc*dx - 4.1e-7*exp(-0.39/(k_B*temp))/(2**0.5)/(alpha**2*beta)*n2*c_sol_n*(1-c_trap2/n2)*vc*dx + c_trap2*v_0*exp(-E2/(k_B*temp))*vc*dx
+ac3,Lc3= lhs(F3),rhs(F3)
+c_trap2=Function(V)
+
+
+
+
+
+
+
+#
+#n3 = TrialFunction(V)#n3 is the density of trap 3
+#Fn3=((n3-n3_n)/dt)*vc*dx-(1-r)*phi/density_W*((1-n3/n3amax)*n3a*fx+(1-n3/n3bmax)*n3b*teta)*vc*dx
+#acn3,Lcn3=lhs(Fn3),rhs(Fn3)
+#n3=Function(V)
+#
+#c_trap3 = TrialFunction(V)#c is the tritium concentration
+#F4=((c_trap3-c_trap3_n)/dt)*vc*dx - 4.1e-7*exp(-0.39/(k_B*temp))/(2**0.5)/(alpha**2*beta)*c_sol_n*(n3_n-c_trap3)*vc*dx + c_trap3*v_0*exp(-E3/(k_B*temp))*vc*dx
+#ac4,Lc4= lhs(F4),rhs(F4)
+#c_trap3=Function(V)
+
+### Time-stepping
 Solute = File("Solutions/Trapping/Solute.pvd")
 Trap1 = File("Solutions/Trapping/Trap1.pvd")
 Trap2 = File("Solutions/Trapping/Trap2.pvd")
@@ -170,54 +193,59 @@ n3F = File("Solutions/Trapping/n3.pvd")
 #fileT = File("solutions/Test/solutionT.pvd")
 filedesorption="Solutions/Trapping/desorption.csv"
 desorption=list()
+total_sol_n=0
+total_trap_n=0
 total_n=0
+tab=[]
 for n in range(num_steps):
   # Update current time
-  print("t= "+str(t)+" s")
-  print("t= "+str(t/3600/24/365.25)+" years")
-  print(str(100*t/Time)+" %")  
+  #print("t= "+str(t)+" s")
+  #print("t= "+str(t/3600/24/365.25)+" years")
+  #print(str(100*t/Time)+" %")  
   t += dt
   phi.t += dt
   temp.t +=dt
-  
 
   # Compute solution concentration
-  
-  
-  solve(ac1==Lc1,c_sol,bcs_c)
-  Solute << (c_sol,t) 
-  
-  #solve(ac2==Lc2,c_trap,bcs_c)
-  #Trap1 << (c_trap,t)
-  
-  #solve(ac3==Lc3,c_trap2,bcs_c)
-  #Trap2 << (c_trap2,t)
+  solve(ac1==Lc1,c_sol,bcs_c)  
+  solve(ac2==Lc2,c_trap,bcs_c)  
+  solve(ac3==Lc3,c_trap2,bcs_c)
 
+  
+  Solute << (c_sol,t)
+  Trap1 << (c_trap,t)
+  Trap2 << (c_trap2,t)
+  
+
+  print('c_trap = '+str(c_trap(size/2)))
+  print('c_trap_n = '+str(c_trap_n(size/2)))
+  print('var c_trap = '+str(c_trap(size/2)-c_trap_n(size/2)))
+
+  print('c_sol = '+str(c_sol(size/2)))
+  print('c_sol_n = '+str(c_sol_n(size/2)))
+  print('var c_sol = '+str(c_sol(size/2)-c_sol_n(size/2)))
+
+
+  A=(c_trap-c_trap_n)
+  #print('Truc chelou = '+str((1-c_trap(size/2)/n1)))
+  c_sol_n.assign(c_sol)
+  c_trap_n.assign(c_trap)
+  
+  #
+
+  #
   #solve(acn3==Lcn3,n3,bcs_c)
   #n3F << (n3,t)
-
+  #
   #solve(ac4==Lc4,c_trap3,bcs_c)
   #Trap3 << (c_trap3,t)
-  
-
-  solve(ac2==Lc2,c_trap,[])
-  Trap1 << (c_trap,t)
-  
-  solve(ac3==Lc3,c_trap2,[])
-  Trap2 << (c_trap2,t)
-
-  solve(acn3==Lcn3,n3,[])
-  n3F << (n3,t)
-
-  solve(ac4==Lc4,c_trap3,[])
-  Trap3 << (c_trap3,t)
 
 
   #Updating boundary conditions
   #bci_c=DirichletBC(V,inside_bc_c,boundary)
-  bcs_c=list()
-  bcs_c.append(bci_c)
-  bcs_c.append(bco_c)
+  #bcs_c=list()
+  #bcs_c.append(bci_c)
+  #bcs_c.append(bco_c)
 
 
   total_trap1=assemble(c_trap*dx)
@@ -228,9 +256,16 @@ for n in range(num_steps):
   total=total_trap+total_sol
   desorption_rate=[-(total-total_n)*density_W/dt,T_var(t),t]
   total_n=total
+  print('Augmentation sol = '+str((total_sol-total_sol_n)/dt))
+  print('Augmentation trap = '+str((total_trap-total_trap_n)/dt))
+  
+
+  total_sol_n=total_sol
+  total_trap_n=total_trap
   if t==implantation_time+resting_time:
-    valeur=total*density_W
+    valeur=(total)*density_W
   if t>implantation_time+resting_time:
+    print(valeur)
     desorption.append(desorption_rate)
     #print('Total of D soluble = ' + str(total_sol))
     #print('Total of D traped 1= ' + str(total_trap1))
@@ -246,8 +281,7 @@ for n in range(num_steps):
     cell_no=cell.index()
     D.vector()[cell_no] = value 
 
-  c_sol_n.assign(c_sol)
-  c_trap_n.assign(c_trap)
+
   c_trap2_n.assign(c_trap2)
   c_trap3_n.assign(c_trap3)
   n3_n.assign(n3)
