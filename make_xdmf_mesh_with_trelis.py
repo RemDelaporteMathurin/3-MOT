f3718dfd52d008a19c465929ce31c13af81ad4b5
Jon <mail@jshimwell.com>
2018-08-08 17:08:59 +0100

    removed exsternal surface finding in xdmf mesher.py

--- make_xdmf_mesh_with_trelis.py
+++ make_xdmf_mesh_with_trelis.py
@@ -18,23 +18,6 @@ def byteify(input):
         return input
 
 
-
-def find_external_surfaces():
-  print('looking for merged surfaces')
-  surfaces_in_all_volumes = cubit.parse_cubit_list("surface"," in volume all ")  
-  list_of_merged_surfaces = []
-  list_of_unmerged_surfaces= []
-  for surface in surfaces_in_all_volumes:
-      is_merged = cubit.is_merged("surface", surface)
-      if is_merged == True:
-        list_of_merged_surfaces.append(surface)
-      elif is_merged == False:
-        cubit.cmd('color surface '+str(surface)+ ' red')
-        list_of_unmerged_surfaces.append(surface)
-  print('list_of_external_surfaces',list_of_unmerged_surfaces)
-  return list_of_unmerged_surfaces
-
-
 def mesh_and_remesh_till_done(quality):
   cubit.cmd('volume all scheme Tetmesh')
   cubit.cmd('volume all scheme tetmesh proximity layers off geometry approximation angle 15 geometric sizing on ')
@@ -59,7 +42,6 @@ else:
 
 cubit.cmd('reset')
 
-print(data['structure_and_materials']['step_files'])
 
 for step_file in data['structure_and_materials']['step_files']:
     print('loading step file ', step_file)
@@ -71,19 +53,16 @@ cubit.cmd('merge tolerance 1.e-6')
 cubit.cmd('merge all')
 
 
-list_of_external_surfaces = find_external_surfaces()
-
 mesh_and_remesh_till_done(data['quality'])
 
-nodes_in_volumes = sorted(cubit.parse_cubit_list("node"," in volume all "))
+nodes_in_volumes = sorted(cubit.parse_cubit_list("node", " in volume all "))
 nodal_coordinates_list = []
-# print('nodes_in_volumes',nodes_in_volumes)
 for node_id in nodes_in_volumes:
     nodal_coordinates_list.append(cubit.get_nodal_coordinates(node_id))
 print(nodal_coordinates_list)
 
 tets_in_volumes = cubit.parse_cubit_list("tet"," in volume all ")
-nodes_in_tets_list=[]
+nodes_in_tets_list = []
 for tet_id in tets_in_volumes:
     nodes_in_tets = cubit.parse_cubit_list("node"," in tet "+str(tet_id))
     nodes_in_tets_list.append(nodes_in_tets)
@@ -95,13 +74,13 @@ f.write('<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>\n')
 f.write('<Xdmf Version="3.0" xmlns:xi="http://www.w3.org/2001/XInclude">\n')
 f.write('  <Domain>\n')
 f.write('    <Grid Name="mesh" GridType="Uniform">\n')
-f.write('      <Topology NumberOfElements="'+str(len(tets_in_volumes))+'" TopologyType="Tetrahedron" NodesPerElement="4">\n')
-f.write('        <DataItem Dimensions="'+str(len(tets_in_volumes))+' 4" NumberType="UInt" Format="XML">\n')
+f.write('      <Topology NumberOfElements="' + str(len(tets_in_volumes)) + '" TopologyType="Tetrahedron" NodesPerElement="4">\n')
+f.write('        <DataItem Dimensions="' + str(len(tets_in_volumes)) + ' 4" NumberType="UInt" Format="XML">\n')
 
 string_to_write_to_file=''
-for tet_id  in tets_in_volumes:
-     nodes_in_tets = cubit.parse_cubit_list("node"," in tet "+str(tet_id))
-     string_to_write_to_file = string_to_write_to_file+'            ' + ' '.join(str(i-1) for i in nodes_in_tets)+'\n'
+for tet_id in tets_in_volumes:
+    nodes_in_tets = cubit.parse_cubit_list("node", " in tet " + str(tet_id))
+    string_to_write_to_file = string_to_write_to_file + '            ' + ' '.join(str(i-1) for i in nodes_in_tets)+'\n'
 f.write(string_to_write_to_file)
 
 f.write('        </DataItem>\n')
@@ -109,9 +88,9 @@ f.write('      </Topology>\n')
 f.write('    <Geometry GeometryType="XYZ">\n')
 f.write('        <DataItem Dimensions="'+str(len(nodal_coordinates_list))+' 3" Format="XML">\n')
 
-string_to_write_to_file=''
+string_to_write_to_file = ''
 for coords in nodal_coordinates_list:
-   string_to_write_to_file= string_to_write_to_file+ '            '+' '.join(str(i) for i in coords)+'\n'
+    string_to_write_to_file = string_to_write_to_file + '            ' + ' '.join(str(i) for i in coords)+'\n'
 f.write(string_to_write_to_file)
 
 f.write('        </DataItem>\n')
@@ -119,14 +98,14 @@ f.write('    </Geometry>\n')
 
 
 
-f.write('      <Attribute Name="volume_marker_material" AttributeType="Scalar" Center="Cell">\n')
-f.write('      <DataItem Dimensions="'+str(len(tets_in_volumes))+' 1" Format="XML">\n')
+f.write('      <Attribute Name="volume_marker_volume_id" AttributeType="Scalar" Center="Cell">\n')
+f.write('      <DataItem Dimensions="'+str(len(tets_in_volumes)) + ' 1" Format="XML">\n')
 all_volumes = cubit.parse_cubit_list("volume", ' all')
-string_to_write_to_file=''
+string_to_write_to_file = ''
 tets_in_each_volume = []
 for tet_id in tets_in_volumes:
-  volume_id = cubit.parse_cubit_list("volume", "in tet "+str(tet_id))[0]
-  string_to_write_to_file += str(volume_id)+'\n'
+    volume_id = cubit.parse_cubit_list("volume", "in tet " + str(tet_id))[0]
+    string_to_write_to_file += str(volume_id) + '\n'
 
 
 f.write(string_to_write_to_file)
@@ -143,31 +122,25 @@ f.write('    <Grid Name="mesh" GridType="Uniform">\n')
 f.write('      <Topology NumberOfElements="'+str(len(triangles_in_tets))+'" TopologyType="Triangle" NodesPerElement="3">\n')
 f.write('      <DataItem Dimensions="'+str(len(triangles_in_tets))+' 3" NumberType="UInt" Format="XML">\n')
 
-string_to_write_to_file=''
+string_to_write_to_file = ''
 for tri_ids in triangles_in_tets:
-    nodes_in_triangles = cubit.parse_cubit_list("node"," in tri "+str(tri_ids))
-    # print(nodes_in_triangles)
-    string_to_write_to_file=string_to_write_to_file+'      '+' '.join(str(i-1) for i in nodes_in_triangles)+'\n'
+    nodes_in_triangles = cubit.parse_cubit_list("node", " in tri " + str(tri_ids))
+    string_to_write_to_file = string_to_write_to_file + '      ' + ' '.join(str(i-1) for i in nodes_in_triangles)+'\n'
 
 f.write(string_to_write_to_file)
 f.write('        </DataItem>\n')
 f.write('    </Topology>\n')
 f.write('      <Geometry Reference="XML">/Xdmf/Domain/Grid/Geometry</Geometry>\n')
 f.write('      <Attribute Name="surface_marker" AttributeType="Scalar" Center="Cell">\n')
-f.write('      <DataItem Dimensions="'+str(len(triangles_in_tets))+' 1" Format="XML">\n')
+f.write('      <DataItem Dimensions="' + str(len(triangles_in_tets)) + ' 1" Format="XML">\n')
 
-
-# print('tri_id_in_special_surfaces',tri_id_in_special_surfaces)
 
 
 string_to_write_to_file = ''
-      
-
 for tri_id in triangles_in_tets:
-  # print(tri_id)
-  surface_id = cubit.parse_cubit_list("surface", " in tri "+str(tri_id))[0]
-  string_to_write_to_file = string_to_write_to_file+'            ' + str(surface_id) + '\n'
-  
+    surface_id = cubit.parse_cubit_list("surface", " in tri " + str(tri_id))[0]
+    string_to_write_to_file = string_to_write_to_file + '            ' + str(surface_id) + '\n'
+
 
 f.write(string_to_write_to_file)
 f.write('        </DataItem>\n')
