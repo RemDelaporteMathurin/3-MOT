aaa8a2cfc2e0c86d493e76e507863a42bd9d992e
RemiTheWarrior <delaporte.mathurin@laposte.net>
2018-08-21 10:50:35 +0100

    CFD works with heat transfers

--- CFD2D.py
+++ CFD2D.py
@@ -1,3 +1,12 @@
+"""
+FEniCS tutorial demo program: Incompressible Navier-Stokes equations
+for channel flow (Poisseuille) on the unit square using the
+Incremental Pressure Correction Scheme (IPCS).
+
+  u' + u . nabla(u)) - div(sigma(u, p)) = f
+                                 div(u) = 0
+"""
+
 from __future__ import print_function
 from fenics import *
 import numpy as np
@@ -9,12 +18,11 @@ mu = 1             # kinematic viscosity
 rho = 1            # density
 
 # Create mesh and define function spaces
-mesh = UnitSquareMesh(20, 20)
+mesh = UnitSquareMesh(40, 40)
 V = VectorFunctionSpace(mesh, 'P', 2)
 Q = FunctionSpace(mesh, 'P', 1)
 
-tol = 0
-
+tol = 1e-14
 class Omega_0(SubDomain):
     def inside(self, x, on_boundary):
         return x[1] <= 0.5 + tol
@@ -38,23 +46,19 @@ dx = Measure('dx', domain=mesh, subdomain_data=subdomains)
 
 
 
-# Define boundaries whole square
-inflow  = 'near(x[0], 0) && x[1]<1'
-outflow = 'near(x[0], 1) && x[1]<1'
+# Define boundaries
+inflow  = 'near(x[0], 0)'
+outflow = 'near(x[0], 1)'
 walls   = 'near(x[1], 0) || near(x[1], 1)'
 
-## Define boundaries whole demi-square
-#inflow  = 'near(x[0], 0) && x[1]<0.5'
-#outflow = 'near(x[0], 1) && x[1]<0.5'
-#walls   = 'near(x[1], 0) || near(x[1], 0.5) || near(x[1], 1) || (near(x[0], 0) && x[1]>0.5) || (near(x[0], 1) && x[1]>0.5) '
-
-borders = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
-
-
+# Define boundaries whole demi-square
+inflow  = 'near(x[0], 0) && x[1]<0.5'
+outflow = 'near(x[0], 1) && x[1]<0.5'
+walls   = 'near(x[1], 0) || near(x[1], 0.5) || near(x[1], 1) || (near(x[0], 0) && x[1]>0.5) || (near(x[0], 1) && x[1]>0.5) '
 
 # Define boundary conditions
 bcu_noslip  = DirichletBC(V, Constant((0, 0)), walls)
-bcp_inflow  = DirichletBC(Q, Constant(8), inflow)
+bcp_inflow  = DirichletBC(Q, Constant(20), inflow)
 bcp_outflow = DirichletBC(Q, Constant(0), outflow)
 bcu = [bcu_noslip]
 bcp = [bcp_inflow, bcp_outflow]
@@ -87,43 +91,22 @@ def epsilon(u):
 def sigma(u, p):
     return 2*mu*epsilon(u) - p*Identity(len(u))
 
-# # # Define variational problem whole square
-
 # Define variational problem for step 1
-F1 = rho*dot((u - u_n) / k, v)*dx + \
-     rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx \
-   + inner(sigma(U, p_n), epsilon(v))*dx \
+F1 = rho*dot((u - u_n) / k, v)*dx(0) + \
+     rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx(0) \
+   + inner(sigma(U, p_n), epsilon(v))*dx(0) \
    + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
-   - dot(f, v)*dx
+   - dot(f, v)*dx(0)
 a1 = lhs(F1)
 L1 = rhs(F1)
 
 # Define variational problem for step 2
-a2 = dot(nabla_grad(p), nabla_grad(q))*dx
-L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx - (1/k)*div(u_)*q*dx
+a2 = dot(nabla_grad(p), nabla_grad(q))*dx(0)
+L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx(0) - (1/k)*div(u_)*q*dx(0)
 
 # Define variational problem for step 3
-a3 = dot(u, v)*dx
-L3 = dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx
-
-# # # Define variational problem demi-square
-
-## Define variational problem for step 1
-#F1 = rho*dot((u - u_n) / k, v)*dx(0) + \
-#     rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx(0) \
-#   + inner(sigma(U, p_n), epsilon(v))*dx(0) \
-#   + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
-#   - dot(f, v)*dx(0)
-#a1 = lhs(F1)
-#L1 = rhs(F1)
-#
-## Define variational problem for step 2
-#a2 = dot(nabla_grad(p), nabla_grad(q))*dx(0)
-#L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx(0) - (1/k)*div(u_)*q*dx(0)
-#
-## Define variational problem for step 3
-#a3 = dot(u, v)*dx(0)
-#L3 = dot(u_, v)*dx(0) - k*dot(nabla_grad(p_ - p_n), v)*dx(0)
+a3 = dot(u, v)*dx(0)
+L3 = dot(u_, v)*dx(0) - k*dot(nabla_grad(p_ - p_n), v)*dx(0)
 
 # Assemble matrices
 A1 = assemble(a1)
@@ -134,20 +117,20 @@ A3 = assemble(a3)
 [bc.apply(A1) for bc in bcu]
 [bc.apply(A2) for bc in bcp]
 
-
-
-wall   = 'near(x[1], 0)'
-
-
-T_n=interpolate(Constant(0.0),Q)
+T = TrialFunction(Q)
+T_n = Function(Q)
 vT = TestFunction(Q)
-T  = TrialFunction(Q)
-bcs_T=[]#[DirichletBC(Q, 8, wall)]
-FT=100*((T-T_n)/dt)*vT*dx(1)+dot(grad(T), grad(vT))*dx(1)-0.1*vT*ds
+FT = ((T-T_n)/k)*vT*dx  # Transient term
+FT += 0.1*dot(grad(T), grad(vT))*dx # Diffusion (conduction term) 
+FT += (dot(u_, grad(T)))*vT*dx(0) # Advection term
+T_ = Function(Q)
 
-output_file=File('solution.pvd')
 
-T=Function(Q)
+bcsT = [DirichletBC(Q, Constant(8), inflow)]
+output_file = File('solution.pvd')
+
+
+
 # Time-stepping
 t = 0
 for n in range(num_steps):
@@ -159,22 +142,22 @@ for n in range(num_steps):
     b1 = assemble(L1)
     [bc.apply(b1) for bc in bcu]
     solve(A1, u_.vector(), b1)
-
+#
     # Step 2: Pressure correction step
     b2 = assemble(L2)
     [bc.apply(b2) for bc in bcp]
     solve(A2, p_.vector(), b2)
-
+#
     # Step 3: Velocity correction step
     b3 = assemble(L3)
     solve(A3, u_.vector(), b3)
 
+    solve(lhs(FT)==rhs(FT), T_, bcsT)
+    T_n.assign(T_)
+
+    output_file << (T_, t)
     output_file << (u_n, t)
-    output_file << (T_n, t)
-
-
-    #solve(lhs(FT) == rhs(FT), T, bcs_T)
     # Update previous solution
-    #T_n.assign(T)
     u_n.assign(u_)
     p_n.assign(p_)
+    #print(u_n(0.5,0.75))
